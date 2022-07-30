4f3c75ca5df0be42ce316951204e9ff601f11709
danugarri <danugarri@hotmail.com>
2022-07-30 16:23:25 +0200

    improved ErrorHandler component

--- src/components/errorHandler/ErrorHandler.tsx
+++ src/components/errorHandler/ErrorHandler.tsx
@@ -2,12 +2,11 @@ import React from 'react';
 import './ErrorHandler.scss';
 
 export const ErrorHandler: React.FC<{}> = () => {
-  const pageReloader = () => window.open(window.location.href, '_self');
+  setTimeout(() => window.open(window.location.href, '_self'), 3000);
   return (
     <div id='error-container'>
       <h1>Se ha producido un error</h1>
       <h3>Volviendo a cargar....</h3>
-      <>{setTimeout(pageReloader, 4000)}</>
     </div>
   );
 };
